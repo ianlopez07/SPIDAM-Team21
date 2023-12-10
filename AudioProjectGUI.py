4edfa00d6d3122e6d08a9dcfcdb2e85b0e26de89
Joe Und <89557578+Pix54@users.noreply.github.com>
2023-12-09 21:41:13 -0500

    more additions to the GUI

--- AudioProjectGUI.py
+++ AudioProjectGUI.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 from tkinter import filedialog
+from file_computation import File_Comp
+from file_handling import File_Handling
+from Plotting import Plot
+import wave
 
 class AudioGUI:
     def __init__(self, root):
@@ -15,8 +19,7 @@ class AudioGUI:
         self.labeltitle = tk.Label(self.root, text="AUDIO CONVERTING AND MODELING GUI")
         self.labeltitle.pack(pady=10, padx=20, anchor="w")
 
-        tk.Label(self.root,
-                 text="").pack()  # These are just to make extra spaces between the labels and buttons on the GUI
+        tk.Label(self.root,text="").pack()  # These are just to make extra spaces between the labels and buttons on the GUI
 
         # LABEL AND BUTTON FOR SELECTING THE FILE
         self.label1 = tk.Label(self.root, text="No file selected")
@@ -29,10 +32,10 @@ class AudioGUI:
         tk.Label(self.root, text="").pack()
 
         # LABEL AND BUTTON TO DISPLAY THE .WAV FILE
-        self.label2 = tk.Label(self.root, text="Display the .Wav")
+        self.label2 = tk.Label(self.root, text="Display the waveform")
         self.label2.pack(pady=10, padx=20, anchor="w")
 
-        self.button2 = tk.Button(self.root, text="Display")
+        self.button2 = tk.Button(self.root, text="Display", command=self.display_waveform)
         self.button2.pack(pady=0, padx=20, anchor="w")
 
         tk.Label(self.root, text="").pack()
@@ -41,7 +44,7 @@ class AudioGUI:
         self.label3 = tk.Label(self.root, text="Display Graphing and Plotting")
         self.label3.pack(pady=10, padx=20, anchor="w")
 
-        self.button3 = tk.Button(self.root, text="Display Graph & Plots")
+        self.button3 = tk.Button(self.root, text="Display Graph & Plots", command=self.display_graphs_and_plots)
         self.button3.pack(pady=0, padx=20, anchor="w")
 
         tk.Label(self.root, text="").pack()
@@ -50,12 +53,27 @@ class AudioGUI:
         self.label4 = tk.Label(self.root, text="Combine the plots")
         self.label4.pack(pady=10, padx=20, anchor="w")
 
-        self.button4 = tk.Button(self.root, text="Combine")
+        self.button4 = tk.Button(self.root, text="Combine", command=self.display_all_reverb)
         self.button4.pack(pady=0, padx=20, anchor="w")
     def open_audio_file(self):
         file_path = filedialog.askopenfilename(filetypes=[("Audio Files", "*.mp3;*.wav")])
         if file_path:
             self.label1.config(text=f"Selected file: {file_path}")
+        handle = File_Handling(file_path)
+
+    def display_waveform(self):
+        file_comp = File_Comp(File_Handling(self.label1.cget("text").replace("Selected File: ", "")))
+        file_comp.wave_file()
+
+    def display_graphs_and_plots(self):
+        plotter = Plot(self.label1.cget("text").replace("Selected file: ", ""))
+        plotter.display_graphs_and_plots()
+
+    def display_all_reverb(self):
+        plotter = Plot(self.label1.cget("text").replace("Selected file: ", ""))
+        plotter.reverb_high()
+        plotter.reverb_mid()
+        plotter.reverb_low()
 
 if __name__ == "__main__":
     root = tk.Tk()
